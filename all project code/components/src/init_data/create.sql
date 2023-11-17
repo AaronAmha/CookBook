00eded4f60079251b4086710ea4da183d4e9f99e
AlexFoucher <alfo8132@colorado.edu>
2023-11-17 13:09:19 -0700

    added recipe table to store from addRecipe

--- all project code/components/src/init_data/create.sql
+++ all project code/components/src/init_data/create.sql
@@ -4,5 +4,14 @@ CREATE TABLE users (
     password CHAR(60) NOT NULL
 );
 
+CREATE TABLE recipes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    ingredients TEXT NOT NULL,
+    instructions TEXT NOT NULL,
+    image_url VARCHAR(255) 
+);
+
+
 insert into users (username, password) values 
 ("andrew", "password") returning * ;
