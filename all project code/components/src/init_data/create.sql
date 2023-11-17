0de8b3a88454d278630d510209dfd8dd6ef9e9e2
Aaron Amha <aaam7416@colorado.edu>
2023-11-17 11:38:37 -0700

    sarah's pull

--- all project code/components/src/init_data/create.sql
+++ all project code/components/src/init_data/create.sql
@@ -1,6 +1,10 @@
 DROP TABLE IF EXISTS users  CASCADE;
 CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    dob DATE NOT NULL,
+    email VARCHAR(200) NOT NULL,
     password CHAR(60) NOT NULL
 );
 
